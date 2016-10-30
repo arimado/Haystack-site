4b69a28da18e29b45fd48b71570a127b5ac9f3e3
doloreel <jarimado@gmail.com>
2016-10-30 22:42:44 +1100

    init

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log('script js loaded');
+
+const concatenateValuesOf = ( obj ) => {
+  console.log(obj)
+  let result
+  for (let key in obj) {
+    result += `${obj[key].reduce((p, c) => p += c + " ")`
+  }
+  console.log(result)
+  return result
+}
+
+const generateCard = ( number ) => {
+  return {
+    baseClasses: [`slide-in`, `card`]
+  , cardNumber: [`${number}`]
+  }
+}
+
+const createCard = ( cardData ) => {
+  const card = document.createElement('div')
+  card.className = concatenateValuesOf(cardData)
+}
+
+const renderCard = ( cardElement ) => {
+}
+
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(generateCard)
+const cardElements = cards.map(createCard)
+
+const cardClass = `card slide-in`
