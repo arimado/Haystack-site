c9cb89faeff8ca6cc304f57fce2e9aca874a07f8
Jeremy Arimado <jarimado@gmail.com>
2016-11-15 07:03:21 +1100

    added mobile and safari support

--- script.js
+++ script.js
@@ -4,80 +4,70 @@ const state = {
 
 // -----------------------------------------------------------------------------
 
-const concatenateValuesOf = ( obj ) => {
-  let result = ''
-  for (let key in obj) {
-    result += `${obj[key].reduce((p, c) => { return p += " " + c + " " })}`
+var concatenateValuesOf = function ( obj ) {
+  var result = ''
+  for (var key in obj) {
+    result += `${obj[key].reduce(function(p, c) { return p += " " + c + " " })}`
   }
   return result
 }
 
-const generateCard = ( number ) => {
+var generateCard = function ( number ) {
   return {
     baseClasses: [`slide-in`, `card`]
   , cardNumber: [`${number}`]
   }
 }
 
-const createCard = ( cardData ) => {
-  const card = document.createElement('div')
+var createCard = function ( cardData ) {
+  var card = document.createElement('div')
   card.className = concatenateValuesOf(cardData)
 }
 
-const renderElement = ( elementData ) => {
-  return () => {
-  }
-}
-
-const renderWith = () => {
-}
-
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(generateCard)
-const cardElements = cards.map(createCard)
+var cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(generateCard)
+var cardElements = cards.map(createCard)
 
 // -----------------------------------------------------------------------------
-
 
 // d_ : dom elements
 // s_ : side effects
 
-const s_resetCards = (cardElements) => {
-  cardElements.forEach((card) => {
+var s_resetCards = function (cardElements) {
+  cardElements.forEach(function(card) {
     card.classList.remove('slide-in-left');
     card.classList.remove('slide-in-right');
   })
 }
 
-const animationDoneCB = ( element, index, elements ) => {
-  return ( e ) => {
+var s_animationDoneCB = function ( element, index, elements ) {
+  return function( e ) {
     if ( elements.length - 1 === index ) {
       s_resetCards(elements);
-      animateCards();
+      s_animateCards();
     }
   }
 }
 
-const d_cards = document.querySelectorAll('.card')
+var d_cards = document.querySelectorAll('.card')
 
-const animateCards = () => {
-  d_cards.forEach((card, idx) => {
-    const cardNo = idx + 1;
+var s_animateCards = function () {
+  Array.prototype.forEach.call(d_cards, function(card, idx) {
+    var cardNo = idx + 1;
     setTimeout(function () {
       if ( idx % 2 === 0 ) {
         card.classList.add('slide-in-right')
       } else {
         card.classList.add('slide-in-left')
       }
-      card.addEventListener("webkitAnimationEnd", animationDoneCB(card, idx, d_cards),false);
-      card.addEventListener("animationend", animationDoneCB(card, idx, d_cards),false);
-      card.addEventListener("oanimationend", animationDoneCB(card, idx, d_cards),false);
+      card.addEventListener("webkitAnimationEnd", s_animationDoneCB(card, idx, d_cards),false);
+      card.addEventListener("animationend", s_animationDoneCB(card, idx, d_cards),false);
+      card.addEventListener("oanimationend", s_animationDoneCB(card, idx, d_cards),false);
     }, cardNo * 5000);
-  });
-}
-
-animateCards();
+  })
+} 
 
 
+s_animateCards();
 
 // add slide toggle in
 // no repeat
